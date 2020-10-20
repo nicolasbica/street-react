cb9b5db97b24419acbaee58b7639923484c4ac63
nicolasbica <swordk664@gmail.com>
2020-10-19 21:37:58 -0300

    projeto street figher react

---
+++ scenes/mainmenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainMenu() {
+    return <div>
+        <h3>
+            Player Select
+        </h3>
+    </div>
+}
+
+export default MainMenu
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainMenu from './scenes/mainmenu'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainMenu />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
