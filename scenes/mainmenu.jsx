e236c30cfe8d790afc785aa26160466e3002cab6
nicolasbica <71796724+nicolasbica@users.noreply.github.com>
2020-10-20 19:27:30 -0300

    Update mainmenu.jsx

--- scenes/mainmenu.jsx
+++ scenes/mainmenu.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 function MainMenu() {
     function Titulo() {
         return <h1>Player Select</h1>
-    }
-    function MapaMundi(pais) {
-        return <>
-            <h1>{pais.nome}</h1></>
-    }
+ 
     function ListaPersonagens({ personagens }) {
         return personagens.map((personagemSelecionado) => {
             return <> {personagemSelecionado.nome} </>
